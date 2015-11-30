80a88eb6cae3993d92c98da388518c007e604ee9
Wilco Bonestroo <wj.bonestroo@student.han.nl>
2015-11-30 15:03:51 +0100

    Updated QuestionDao test.

--- src/test/java/nl/halewijn/persoonlijkheidstest/datasource/dao/QuestionDaoTest.java
+++ src/test/java/nl/halewijn/persoonlijkheidstest/datasource/dao/QuestionDaoTest.java
@@ -1,127 +1,119 @@
-//package nl.halewijn.persoonlijkheidstest.datasource.dao;
-//
-//import static org.junit.Assert.*;
-//
-//import java.util.List;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import nl.halewijn.persoonlijkheidstest.Application;
-//import nl.halewijn.persoonlijkheidstest.domain.PersonalityType;
-//import nl.halewijn.persoonlijkheidstest.domain.Question;
-//import nl.halewijn.persoonlijkheidstest.domain.Theorem;
-//import nl.halewijn.persoonlijkheidstest.domain.TheoremBattle;
-//
-//@Transactional
-//@RunWith(SpringJUnit4ClassRunner.class)
-////@ContextConfiguration("applicationContext.xml")
-////@ContextConfiguration(classes = {TestDatabaseConfig.class})
-//@SpringApplicationConfiguration(Application.class)
-////@WebIntegrationTest
-//public class QuestionDaoTest {
-//	@Autowired
-//	private QuestionDao dao;
-//	private PersonalityTypeDao daoPersonality;
-//	private TheoremDao daoTheorem;
-//	
-////	@Before
-////	public void setUpDB(){
-////		
-////	}
-////	
-////	@After
-////	public void tearDownDB(){
-////		
-////	}
-////	
-////	
-////	public Session testGetSession(){
-////		return sessionFactory.getCurrentSession();
-////	}
-//	
-//	@Test
-//	public void testSave(){
-//		PersonalityType perfectionist = daoPersonality.getById(1);
-//		Theorem theorem1 = daoTheorem.getById(1);
-//		Theorem theorem2 = daoTheorem.getById(2);
-//		Question Question = new Question();
-//		dao.save(theoremBattle);
-//		TheoremBattle insertedTheoremBattle = dao.getById(theoremBattle.getID());
-//		assertEquals(insertedTheoremBattle.getID(), theoremBattle.getID());
-//		assertEquals("TestTheoremBattle", insertedTheoremBattle.getText());
-//		assertEquals(theorem1, insertedTheoremBattle.getFirstTheorem());
-//		assertEquals(theorem2, insertedTheoremBattle.getSecondTheorem());
-//	}
-//	
-//	@Test
-//	public void testGetAll(){
-//		List<Question> results = daoQuestion.getAll();
-//		for (int i = 1; i <= results.size(); i ++){
-//			assertEquals(i, results.get(i-1).getID());
-//		}
-//	}
-//	
-//	@Test
-//	public void testGetById(){
-//		int theoremID = 1;
-//		TheoremBattle battle = dao.getById(theoremID);
-//		assertNotNull(battle);
-//		assertEquals("Perfectionist", battle.getText());
-//	}
-//	
-//	@Test
-//	public void testGetTypeById(){
-//		
-//	}
-//	
-//	@Test
-//	public void testUpdate(){
-//		PersonalityType perfectionist = daoPersonality.getById(1);
-//		Theorem theorem1 = daoTheorem.getById(1);
-//		Theorem theorem2 = daoTheorem.getById(2);
-//		Theorem newTheorem1 = daoTheorem.getById(3);
-//		Theorem newTheorem2 = daoTheorem.getById(4);
-//		TheoremBattle toUpdate;
-//		toUpdate = dao.getById(10);
-//		assertNotNull(toUpdate);
-//		assertEquals("TestTheoremBattle", toUpdate.getText());
-//		assertEquals(theorem1, toUpdate.getFirstTheorem());
-//		assertEquals(theorem2, toUpdate.getSecondTheorem());
-//		
-//		toUpdate.setText("UpdatedTestTheoremBattle");
-//		toUpdate.setFirstTheorem(newTheorem1);
-//		toUpdate.setSecondTheorem(newTheorem2);
-//		dao.update(toUpdate);
-//		
-//		TheoremBattle updated;
-//		updated = dao.getById(10);
-//		assertEquals("UpdatedTestTheoremBattle", updated.getText());
-//		assertEquals(newTheorem1, updated.getFirstTheorem());
-//		assertEquals(newTheorem2, updated.getSecondTheorem());
-//	}
-//	
-//	@Test
-//	public void testDelete(){
-//		PersonalityType perfectionist = daoPersonality.getById(1);
-//		TheoremBattle toDelete;
-//		toDelete = dao.getById(10);
-//		Theorem theorem1 = toDelete.getFirstTheorem();
-//		Theorem theorem2 = toDelete.getSecondTheorem();
-//		assertNotNull(toDelete);
-//		assertEquals("TestTheoremBattle", toDelete.getText());
-//		assertEquals(theorem1, toDelete.getFirstTheorem());
-//		assertEquals(theorem2, toDelete.getSecondTheorem());
-//		
-//		// Delete TheoremBattle.
-//		dao.delete(toDelete);
-//		// Check if TheoremBattle is deleted.
-//		TheoremBattle deleted = dao.getById(10);
-//		assertNull(toDelete);
-//		assertNull(deleted);
-//	}
-//}
+package nl.halewijn.persoonlijkheidstest.datasource.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import nl.halewijn.persoonlijkheidstest.Application;
+import nl.halewijn.persoonlijkheidstest.domain.OpenQuestion;
+import nl.halewijn.persoonlijkheidstest.domain.PersonalityType;
+import nl.halewijn.persoonlijkheidstest.domain.Question;
+import nl.halewijn.persoonlijkheidstest.domain.Theorem;
+import nl.halewijn.persoonlijkheidstest.domain.TheoremBattle;
+import nl.halewijn.persoonlijkheidstest.services.local.LocalPersonalityTypeService;
+import nl.halewijn.persoonlijkheidstest.services.local.LocalQuestionService;
+import nl.halewijn.persoonlijkheidstest.services.local.LocalTheoremService;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@ActiveProfiles("test")
+public class QuestionDaoTest {
+	
+	@Autowired
+	private LocalQuestionService localQuestionService;
+	
+	@Autowired
+	private LocalPersonalityTypeService localPersonalityTypeService;
+	
+	@Autowired
+	private LocalTheoremService localTheoremService;
+	
+	@Test
+	public void testSave(){
+		PersonalityType perfectionist = new PersonalityType("Perfectionist", "first", "second");
+		localPersonalityTypeService.save(perfectionist);
+		
+		Theorem theorem1 = new Theorem(perfectionist, "Stelling", 1.2);
+		localTheoremService.save(theorem1);
+		Theorem theorem2 = new Theorem(perfectionist, "Stelling2", 1.2);
+		localTheoremService.save(theorem2);
+		
+		TheoremBattle theoremBattle = new TheoremBattle("TestTheoremBattle", theorem1, theorem2);
+		localQuestionService.save(theoremBattle);
+		
+		TheoremBattle insertedTheoremBattle = (TheoremBattle) localQuestionService.getById(theoremBattle.getID());
+		assertEquals(insertedTheoremBattle.getID(), theoremBattle.getID());
+		assertEquals("TestTheoremBattle", insertedTheoremBattle.getText());
+	}
+	
+	@Test
+	public void testGetAll(){
+		List<Question> newResults = localQuestionService.getAll();
+		
+		OpenQuestion newQuestion = new OpenQuestion("Nieuwe vraag");
+		localQuestionService.save(newQuestion);
+		newResults.add(newQuestion);
+		
+		List<Question> results = localQuestionService.getAll();
+		assertEquals(newResults.size(), results.size());		
+	}
+
+	@Test
+	public void testGetById(){
+		OpenQuestion newQuestion = new OpenQuestion("Nieuwe vraag...");
+		localQuestionService.save(newQuestion);
+		
+		Question question = localQuestionService.getById(2);
+		assertEquals(newQuestion.getText(), question.getText());
+	}
+	
+	@Test
+	public void testGetTypeById(){
+		PersonalityType perfectionist = new PersonalityType("Perfectionist", "first", "second");
+		localPersonalityTypeService.save(perfectionist);
+		
+		Theorem theorem1 = new Theorem(perfectionist, "Stelling", 1.2);
+		localTheoremService.save(theorem1);
+		Theorem theorem2 = new Theorem(perfectionist, "Stelling2", 1.2);
+		localTheoremService.save(theorem2);
+		
+		TheoremBattle theoremBattle = new TheoremBattle("TestTheoremBattle", theorem1, theorem2);
+		localQuestionService.save(theoremBattle);
+		
+		OpenQuestion openQuestion = new OpenQuestion("Een nieuwe open vraag");
+		localQuestionService.save(openQuestion);
+		
+		assertEquals("nl.halewijn.persoonlijkheidstest.domain.TheoremBattle",localQuestionService.getTypeById(theoremBattle.getID()));
+		assertEquals("nl.halewijn.persoonlijkheidstest.domain.OpenQuestion",localQuestionService.getTypeById(openQuestion.getID()));
+	}
+
+	@Test
+	public void testUpdate(){
+		OpenQuestion openQuestion = new OpenQuestion("Nieuwe vraag");
+		localQuestionService.save(openQuestion);
+		assertEquals("Nieuwe vraag", openQuestion.getText());
+		
+		openQuestion.setText("nieuwe tekst");
+		localQuestionService.update(openQuestion);
+		assertEquals("nieuwe tekst", openQuestion.getText());
+	}
+	
+	@Test
+	public void testDelete(){
+		OpenQuestion openQuestion = new OpenQuestion("Nieuwe vraag");
+		localQuestionService.save(openQuestion);
+		assertEquals("Nieuwe vraag", openQuestion.getText());
+		
+		localQuestionService.delete(openQuestion);
+		assertNull(openQuestion);
+	}
+}
