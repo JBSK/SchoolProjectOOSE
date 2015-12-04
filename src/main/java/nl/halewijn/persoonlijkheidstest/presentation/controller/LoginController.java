812f6b76c1e7274c6f42064735aac1f963d40ae2
Duncan <lyraeduncan@hotmail.com>
2015-12-04 09:52:53 +0100

    Inloggen en uitloggen werkt. Ervoor gezorgd dat de formulieren op login pagina kloppen. Ervoor gezorgd dat de header een uitloggen knop weergeeft als je ingelogd bent, en vice versa. Tijdens het inloggen wordt er door de back-end gecontroleerd of het ingevoerde emailadres bestaat, en zo ja, of het bijbehorende gehashte wachtwoord klopt.

--- src/main/java/nl/halewijn/persoonlijkheidstest/presentation/controller/LoginController.java
+++ src/main/java/nl/halewijn/persoonlijkheidstest/presentation/controller/LoginController.java
@@ -1,12 +1,28 @@
 package nl.halewijn.persoonlijkheidstest.presentation.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import nl.halewijn.persoonlijkheidstest.domain.User;
+import nl.halewijn.persoonlijkheidstest.services.PasswordHash;
+import nl.halewijn.persoonlijkheidstest.services.local.LocalUserService;
+
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private LocalUserService localUserService;
+	
+	private PasswordHash passwordHash = new PasswordHash();
+	
 	/**
 	 * If the file path relative to the base was "/login", return the "login" web page.
 	 */
@@ -14,4 +30,41 @@ public class LoginController {
     public String login(Model model) {
        return "login";
     }
+	
+	/**
+	 * If someone presses the login button, check in the database whether this user exists.
+	 * 
+	 * If the user exists, check whether or not the password is correct.
+	 * If the password is correct, add the user's email to the session, and redirect to the home page.
+	 * 
+	 * If the user doesn't exist, or the password is incorrect, return to the login page.
+	 */
+	@RequestMapping(value="/loginCheck", method=RequestMethod.POST)
+	public String loginCheck(Model model, HttpSession session, HttpServletRequest req) {
+		String email = req.getParameter("email");
+		User user = localUserService.findByName(email);
+		
+		if (user != null) {
+			boolean correctPassword = passwordHash.verifyPassword(req.getParameter("password"), user.getPassword());
+			
+			if(user.getEmailAddress() != null && correctPassword) {
+				session.setAttribute("email", user.getEmailAddress());
+			} else {
+				return "login";
+			}
+		} else {
+			return "login";
+		}
+		return "redirect:/";
+	}
+	
+	/**
+	 * If a logged in user presses the logout button, remove the user's email from the session.
+	 */
+	@RequestMapping(value="/loguit", method=RequestMethod.GET)
+	public String loguit(Model model, HttpSession session) {
+		session.setAttribute("email", null);
+		session.removeAttribute("email");
+		return "redirect:/";
+	}
 }
