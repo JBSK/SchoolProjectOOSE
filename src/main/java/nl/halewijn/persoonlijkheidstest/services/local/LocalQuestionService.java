2ca1346d992a858194f6576401a2d19493f5e8b1
Wilco Bonestroo <wj.bonestroo@student.han.nl>
2015-12-10 15:38:47 +0100

    Changed foreign key of firstTheorem and secondTheorem to a OneToOne relation in TheoremBattle class.

--- src/main/java/nl/halewijn/persoonlijkheidstest/services/local/LocalQuestionService.java
+++ src/main/java/nl/halewijn/persoonlijkheidstest/services/local/LocalQuestionService.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class LocalQuestionService implements IQuestionService  {
