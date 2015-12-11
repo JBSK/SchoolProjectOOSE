1855deb307980a7eeed5e802000b5121d1d30d97
Wilco Bonestroo <wj.bonestroo@student.han.nl>
2015-12-11 13:36:38 +0100

    Added findAll and findAllByText test to LocalQuestionServiceTest.

--- src/test/java/nl/halewijn/persoonlijkheidstest/services/local/LocalQuestionServiceTest.java
+++ src/test/java/nl/halewijn/persoonlijkheidstest/services/local/LocalQuestionServiceTest.java
@@ -188,7 +188,29 @@ public class LocalQuestionServiceTest {
     	
     	localQuestionService.sortQuestionsArray(questionListSorted);
     	assertEquals(newQuestion2, questionListSorted.get(0));
+    	questionListSorted.get(0).setQuestionId(1);
+    	questionListSorted.get(1).setQuestionId(1);
     	
+    	localQuestionService.sortQuestionsArray(questionListSorted);
+    	assertEquals(newQuestion2, questionListSorted.get(0));
+    }
+    
+    public void findAllTest() {
+    	OpenQuestion questionOne = new OpenQuestion("1");
+    	OpenQuestion questionTwo = new OpenQuestion("2");
+    	int questionsAmount = localQuestionService.findAll().size();
+    	localQuestionService.save(questionOne);
+    	localQuestionService.save(questionTwo);
+    	assertEquals(questionsAmount+2, localQuestionService.findAll().size());
+    }
+    
+    public void findAllByTextTest() {
+    	OpenQuestion questionOne = new OpenQuestion("Specific text");
+    	OpenQuestion questionTwo = new OpenQuestion("Specific text");
+    	int questionsAmount = localQuestionService.findAllByText("Specific text").size();
+    	localQuestionService.save(questionOne);
+    	localQuestionService.save(questionTwo);
+    	assertEquals(questionsAmount+2, localQuestionService.findAllByText("Specific text").size());
     }
     
 }
