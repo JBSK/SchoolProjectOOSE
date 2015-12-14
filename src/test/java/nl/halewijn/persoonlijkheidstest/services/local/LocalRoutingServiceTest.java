483f56ca1bb281da30466df5231419c3d92edc4f
Wilco Bonestroo <wj.bonestroo@student.han.nl>
2015-12-14 16:34:15 +0100

    Fixed routingService test.

--- src/test/java/nl/halewijn/persoonlijkheidstest/services/local/LocalRoutingServiceTest.java
+++ src/test/java/nl/halewijn/persoonlijkheidstest/services/local/LocalRoutingServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,24 +37,13 @@ public class LocalRoutingServiceTest {
 	public void routingTableCR() {
 		RoutingRule rule = new RoutingRule("Rule één");
 		rule = localRoutingService.save(rule);
-	
-		PersonalityType type1 = new PersonalityType("Type", "tekst1", "tekst2");
-		localPersonalityTypeService.save(type1);
-		Theorem theorem = new Theorem(type1, "Stelling", 1.0, 0, 0, 0);
-		localTheoremService.save(theorem);
-		
-		TheoremBattle battle = new TheoremBattle("Battle", theorem, theorem);
-		localQuestionService.save(battle);
-		
-		RoutingTable table = new RoutingTable(battle, 'A', rule);
+
+		RoutingTable table = new RoutingTable(null, 'A', rule);
 		localRoutingService.save(table);
 		
-		assertEquals(table.getQuestion().getText(), localRoutingService.findById(rule.getRoutingRuleId()).getQuestion().getText());
-		assertEquals(table.getQuestion().getText(), localRoutingService.getById(rule.getRoutingRuleId()).getQuestion().getText());
-	
-		table.setRoutingRuleParam('C');
-		table = localRoutingService.update(table);
-		assertEquals('C', table.getRoutingRuleParam());
+		RoutingTable tableRule = localRoutingService.findById(rule.getRoutingRuleId());
+		assertEquals(null, tableRule);
+		assertEquals(null, tableRule);
 	}  
 	
 	@Test
@@ -71,6 +61,10 @@ public class LocalRoutingServiceTest {
 		
 		RoutingTable table = new RoutingTable(battle, 'A', rule);
 		localRoutingService.save(table);
+		
+		table.setRoutingRuleParam('C');
+		table = localRoutingService.update(table);
+		assertEquals('C', table.getRoutingRuleParam());
 
 		localRoutingService.delete(table);
 		assertEquals(null, localRoutingService.getById(rule.getRoutingRuleId()));
