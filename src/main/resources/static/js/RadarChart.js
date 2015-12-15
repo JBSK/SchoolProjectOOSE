29ae6fdbe7ae35fb35b23b3d3908e3e44f3e516d
Jelle Dekker <JM.Dekker1@student.han.nl>
2015-12-15 16:34:31 +0100

    Refactored radar graph color code Changed the placement of the social media buttons

--- src/main/resources/static/js/RadarChart.js
+++ src/main/resources/static/js/RadarChart.js
@@ -276,4 +276,13 @@ function RadarChart(id, data, options) {
 	  });
 	}//wrap	
 	
-}//RadarChart
+} //RadarChart
+
+function addRadarGraphLabelColors(){
+    var elements = document.getElementsByClassName("legend");
+    elements[0].setAttribute("class", "legend label-" + elements.length);
+    for (var i = 1; i < elements.length; i++) {
+        elements[i].setAttribute("class", "legend label-"+i);
+    }
+}
+window.addEventListener("load", function() { addRadarGraphLabelColors(); });
