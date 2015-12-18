ba6a253e0c2455e257c5a503df911e3b551fb0af
Jelle Dekker <JM.Dekker1@student.han.nl>
2015-12-18 12:49:28 +0100

    Removed unused imports

--- src/main/java/nl/halewijn/persoonlijkheidstest/Application.java
+++ src/main/java/nl/halewijn/persoonlijkheidstest/Application.java
@@ -2,17 +2,12 @@ package nl.halewijn.persoonlijkheidstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
