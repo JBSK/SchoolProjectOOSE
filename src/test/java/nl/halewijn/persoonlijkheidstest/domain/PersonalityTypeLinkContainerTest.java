6bc3880e93cf20fa71b02da9f0cd5a882495a583
Duncan <lyraeduncan@hotmail.com>
2015-12-18 12:47:02 +0100

    Added a few more (useless) tests for domain getters and setters.

---
+++ src/test/java/nl/halewijn/persoonlijkheidstest/domain/PersonalityTypeLinkContainerTest.java
@@ -0,0 +1,34 @@
+package nl.halewijn.persoonlijkheidstest.domain;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import nl.halewijn.persoonlijkheidstest.Application;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@ActiveProfiles("test")
+public class PersonalityTypeLinkContainerTest {
+	
+	@Test
+	public void gettersAndSettersTest() {
+		PersonalityType type = new PersonalityType();
+		PersonalityTypeLink ptl = new PersonalityTypeLink();
+		List<PersonalityTypeLink> ptlList = new ArrayList<>();
+		ptlList.add(ptl);
+		
+		PersonalityTypeLinkContainer ptlc = new PersonalityTypeLinkContainer(type, ptlList);
+		assertEquals(type, ptlc.getPersonalityType());
+		assertEquals(ptlList, ptlc.getLinks());
+	}
+}
