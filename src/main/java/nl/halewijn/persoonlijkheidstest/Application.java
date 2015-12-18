df31ed5967fae14c1444596954f1fdadd7c2fa41
Jelle Dekker <JM.Dekker1@student.han.nl>
2015-12-18 12:47:09 +0100

    Added custom 404 error page, but it doesn't display the error yet.

--- src/main/java/nl/halewijn/persoonlijkheidstest/Application.java
+++ src/main/java/nl/halewijn/persoonlijkheidstest/Application.java
@@ -2,10 +2,32 @@ package nl.halewijn.persoonlijkheidstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/errorPage.html");
+                container.addErrorPages(error404Page);
+            }
+        };
+    }
 
     @PersistenceConstructor
 	public Application() {
