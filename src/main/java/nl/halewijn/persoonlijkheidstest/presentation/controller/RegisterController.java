be2ec6801bb618e31d8eac5444005af938749710
Jelle Dekker <JM.Dekker1@student.han.nl>
2016-01-04 12:33:01 +0100

    Removed unused imports

--- src/main/java/nl/halewijn/persoonlijkheidstest/presentation/controller/RegisterController.java
+++ src/main/java/nl/halewijn/persoonlijkheidstest/presentation/controller/RegisterController.java
@@ -3,9 +3,7 @@ package nl.halewijn.persoonlijkheidstest.presentation.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-import net.sf.json.JSONSerializer;
 import nl.halewijn.persoonlijkheidstest.domain.Result;
 import nl.halewijn.persoonlijkheidstest.services.Constants;
 import nl.halewijn.persoonlijkheidstest.services.local.LocalButtonService;
@@ -14,7 +12,6 @@ import nl.halewijn.persoonlijkheidstest.services.local.LocalResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,7 +24,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.List;
 
 @Controller
 public class RegisterController {
